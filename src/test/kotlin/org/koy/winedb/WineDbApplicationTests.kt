68e9bdb96637982e1afbf6e545f4d2ba1ff4cc7e
= <=>
2022-04-18 22:03:52 -0500

    first commit

---
+++ src/test/kotlin/org/koy/winedb/WineDbApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.koy.winedb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WineDbApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
